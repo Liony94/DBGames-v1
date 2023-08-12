e4c921cb409b2682ef0a430e321c86f12f3edaf6
Liony94 <gui.prouchandy@gmail.com>
2023-08-12 03:04:55 +0200

    Ajout des images sur home

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,11 @@ Encore
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
+    .copyFiles({
+        from: './assets/images',
+        pattern: /.(png|jpg|jpeg|svg|gif)$/,
+        to: 'images/[path][name].[ext]',
+    })
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
