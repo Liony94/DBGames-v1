639407a1272033fd773a4bb70f06c081093d3347
Liony94 <gui.prouchandy@gmail.com>
2023-08-19 22:51:27 +0200

    fix front + modal false

--- assets/js/message.js
+++ assets/js/message.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let conversationList = document.getElementById("conversationList");
+    let messageDisplay = document.getElementById("messageDisplay");
+
+    conversationList.addEventListener("click", function(event) {
+        let conversationElement = event.target.closest("li");
+        if (!conversationElement) return;
+
+        let username = conversationElement.querySelector("h3").textContent;
+        let title = conversationElement.querySelector(".title").textContent;
+        let content = conversationElement.querySelector(".content").textContent;
+
+        messageDisplay.querySelector("h3").textContent = username;
+        messageDisplay.querySelector("h1").textContent = title;
+        messageDisplay.querySelector("article").textContent = content;
+    });
+});
 
 document.getElementById("sendButton").addEventListener("click", function(event) {
     event.preventDefault();
